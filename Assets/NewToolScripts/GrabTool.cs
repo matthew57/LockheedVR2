a20ead55d413cea2e01f6de8a2b16840f207b352
matthew57 <nosnhojdm@gmail.com>
2016-12-13 15:04:45 -0700

    scene tweaks

--- Assets/NewToolScripts/GrabTool.cs
+++ Assets/NewToolScripts/GrabTool.cs
@@ -19,10 +19,10 @@ public class GrabTool : MonoBehaviour {
 
 	public void buttonPressed(bool timeNormal)
 	{
-		
-		currentBolt = (GameObject)Instantiate(Bolt,this.transform.position + this.transform.rotation * (BoltPoint ), this.transform.rotation *Quaternion.Euler(90,0,0)  );
-		currentBolt.transform.SetParent (this.transform);
-
+		if (Bolt) {
+			currentBolt = (GameObject)Instantiate (Bolt, this.transform.position + this.transform.rotation * (BoltPoint), this.transform.rotation * Quaternion.Euler (90, 0, 0));
+			currentBolt.transform.SetParent (this.transform);
+		}
 	}
 
 	public void buttonReleased(bool timeNormal)
