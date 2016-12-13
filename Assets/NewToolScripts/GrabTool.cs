d1f0edf6db5550f4fbbacefbc7bfe6cb761459d1
matthew57 <nosnhojdm@gmail.com>
2016-12-13 14:12:38 -0700

    Pick Bug fix, Layers back in
    Somehow all of our layers got deleted?????

--- Assets/NewToolScripts/GrabTool.cs
+++ Assets/NewToolScripts/GrabTool.cs
@@ -6,27 +6,38 @@ public class GrabTool : MonoBehaviour {
 	public Vector3 grabPointA;
 	public float handleAngle;
 	public int grabAnim;
+	public Vector3 BoltPoint;
 
+	public GameObject Bolt;
+
+	GameObject currentBolt;
 	public Vector3 getGrabPoint()
 	{
 		return grabPointA ;
 	}
 
 
-	public void buttonPressed()
+	public void buttonPressed(bool timeNormal)
 	{
-		Debug.Log ("Button pressed");
+		
+		currentBolt = (GameObject)Instantiate(Bolt,this.transform.position + this.transform.rotation * (BoltPoint ), this.transform.rotation *Quaternion.Euler(90,0,0)  );
+		currentBolt.transform.SetParent (this.transform);
+
 	}
 
-	public void buttonReleased()
+	public void buttonReleased(bool timeNormal)
 	{
-		Debug.Log ("Button Release");
+		if (currentBolt) {
+			currentBolt.transform.SetParent (null);
+		}
+
 
 	}
 
 	void OnDrawGizmos()
 	{
 		Gizmos.DrawSphere ((this.transform.position + this.transform.rotation * (grabPointA )),.04f);
+		Gizmos.DrawSphere ((this.transform.position + this.transform.rotation * (BoltPoint )),.04f);
 	
 	}
 
